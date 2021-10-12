da21cd0876856a59084fcc1b24f2a3b5e3ab428b
Ignas Biekša <ignasbieksa@gmail.com>
2021-10-12 16:02:27 +0300

    gradient fixes

--- app.js
+++ app.js
@@ -7,18 +7,15 @@ let tfront = document.getElementById('tFront');
 let bottomPart = document.getElementById('sec');
 let ground = document.getElementById('ground');
 let gradient = document.getElementById('gradient');
-let backPart = document.getElementById('darkback');
 let aboutbutton = document.getElementById('about');
 let homebutton = document.getElementById('home');
 let contactsbutton = document.getElementById('contacts');
-
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     ground.style.top = value*-0.9+'px';
     gradient.style.top =790+(value*-0.9)+'px';
     bottomPart.style.top = value*-0.9+'px';
-    backPart.style.height = value+'px';
     mfront.style.top = value*-0.2+'px';
     tback.style.top = value*-0.3+'px';
     tfront.style.top = 20+(value*-0.4)+'px';
