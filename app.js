fd7a1de321e96a7f416d5742eb3ff596c38cb01e
Ignas Biekša <ignasbieksa@gmail.com>
2021-10-13 14:30:30 +0300

    fixes for parallax

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ let tfront = document.getElementById('tFront');
 let bottomPart = document.getElementById('sec');
 let ground = document.getElementById('ground');
 let gradient = document.getElementById('gradient');
-
+let blackbox = document.getElementById('blackBox');
+let element = document.getElementsByClassName('sec');
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
-    ground.style.top = value*-0.7+'px';
-    gradient.style.top =790+(value*-0.7)+'px';
-    bottomPart.style.top = value*-0.7+'px';
+    ground.style.top = value*-0.5+'px';
+    // gradient.style.top =790+(value)+'px';
+    // bottomPart.style.top = value+'px';
     mfront.style.top = value*-0.2+'px';
     tback.style.top = value*-0.3+'px';
-    tfront.style.top = 20+value*-0.4+'px';
+    tfront.style.top = value*-0.4+'px';
+    blackbox.style.top = value*-0.5+'px';
     birds.style.top = value*-0.1 +'px';
+    element.style.height+=value*0.5 + 'px';
 })
